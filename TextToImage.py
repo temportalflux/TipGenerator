5a1c2748087a253d849741275f742d0c816e3c3b
temportalflux <dustin.yost.t@gmail.com>
2019-08-23 08:07:24 -0400

    Add basic assest for generating tips

---
+++ TextToImage.py
@@ -0,0 +1,28 @@
+import numpy as np
+import cv2
+
+# Create a black image
+img = np.zeros((512,512,3), np.uint8)
+
+# Write some Text
+
+font                   = cv2.FONT_HERSHEY_SIMPLEX
+bottomLeftCornerOfText = (10,500)
+fontScale              = 1
+fontColor              = (255,255,255)
+lineType               = 2
+
+cv2.putText(img,'Hello World!', 
+    bottomLeftCornerOfText, 
+    font, 
+    fontScale,
+    fontColor,
+    lineType)
+
+#Display the image
+cv2.imshow("img",img)
+
+#Save image
+cv2.imwrite("out.jpg", img)
+
+cv2.waitKey(0)
