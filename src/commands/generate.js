ba8afb4b25d83c528e3571575ed33b1ec0298e0b
temportalflux <dustin.yost.t@gmail.com>
2019-11-02 09:45:21 -0400

    Add the option to `generate` to post in a specific channel

--- src/commands/generate.js
+++ src/commands/generate.js
@@ -1,5 +1,7 @@
+const lodash = require('lodash');
+
 module.exports = {
-	command: 'generate [trackUsage]',
+	command: 'generate [trackUsage] [channel]',
 	desc: 'Generate a tip with image.',
 	builder: {
 		trackUsage: {
@@ -10,11 +12,42 @@ module.exports = {
 	},
 	handler: async (argv) =>
 	{
-		await argv.application.generateTipScreen(
-			argv.message.guild,
-			argv.application,
-			argv.message.channel,
-			argv.trackUsage
-		);
+		let outputChannel = argv.message.channel;
+
+		if (argv.channel)
+		{
+			if (!argv.message.guild.available)
+			{
+				await argv.message.reply(`Failed to find channel with the name ${argv.channel}, guild is not available`);
+				return;
+			}
+			const result = lodash.find(
+				lodash.toPairs(argv.message.guild.channels),
+				([id, channel]) => {
+					return channel.type === 'text' && channel.name === argv.channel;
+				}
+			);
+			if (!result)
+			{
+				await argv.message.reply(`No such channel with the name ${argv.channel}.`);
+				return;
+			}
+			const [channelId, channel] = result;
+			outputChannel = channel;
+			// TODO: Check permissions on the channel
+		}
+
+		// This try is just to ensure the app doesn't crash if it doesn't have permission to post in that channel
+		try
+		{
+			await argv.application.generateTipScreen(
+				argv.message.guild,
+				argv.application,
+				outputChannel,
+				argv.trackUsage
+			);
+		}
+		catch(e)
+		{}
 	},
 };
